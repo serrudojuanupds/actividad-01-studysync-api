f4174b3ef238ad987b271320566840e9ed255bf9
serrudojuanupds <juanelvicio1@gmail.com>
2026-05-18 17:59:44 -0400

    feat: agregar mas sesiones iniciales de ejemplo

--- src/models/sesiones.model.js
+++ src/models/sesiones.model.js
@@ -18,7 +18,38 @@ let sesiones = [
     lugar: "Sala de estudio",
     cupos: 4,
     completada: false
+  },
+    {
+    id: 3,
+    titulo: "Preparación para defensa de API REST",
+    materia: "Programación IV",
+    fecha: "2026-05-24",
+    hora: "17:30",
+    lugar: "Laboratorio de Sistemas",
+    cupos: 6,
+    completada: false
+  },
+  {
+    id: 4,
+    titulo: "Práctica de Git y GitHub",
+    materia: "Programación IV",
+    fecha: "2026-05-26",
+    hora: "18:30",
+    lugar: "Biblioteca UPDS",
+    cupos: 8,
+    completada: false
+  },
+  {
+    id: 5,
+    titulo: "Repaso de despliegue en Render",
+    materia: "Desarrollo Web",
+    fecha: "2026-05-28",
+    hora: "19:00",
+    lugar: "Sala de estudio",
+    cupos: 5,
+    completada: false
   }
+  
 ];
 
 module.exports = sesiones;
